841b7c08c0d079e98db3753597c2cde75d2ed354
Nyao0410 <pentyan0410@gmail.com>
2025-08-05 13:54:08 +0900

    v0.1.1　Coding with PS

---
+++ Skyscraper/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:skyscraper/router/app_router.dart';
+
+void main() {
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final GoRouter router = ref.watch(appRouterProvider);
+
+    return MaterialApp.router(
+      routerConfig: router,
+      title: 'Skyscraper',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
